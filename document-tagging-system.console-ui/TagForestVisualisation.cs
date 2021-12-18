a9f459e59b65015d384a4e9a2a3e6e82559d3871
Joseph Wakeling <josephjwakeling@gmail.com>
2021-12-18 21:05:57 +0000

    write logic for displaying tags and documents as a forest

---
+++ document-tagging-system.console-ui/TagForestVisualisation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace document_tagging_system.console_ui
+{
+    public class TagForestVisualisation
+    {
+        private readonly IEnumerable<ITagBranch> _roots;
+
+        public void Show()
+        {
+            foreach (var root in _roots)
+            {
+                Console.WriteLine(root.Tag().FullText() + ":");
+                ShowContent(root, 1);
+            }
+        }
+
+        //TODO: find more OO way of doing this
+        private void ShowContent(ITagBranch branch, int indent)
+        {
+            foreach (var document in branch.Documents())
+            {
+                Console.Write(new string(' ', indent + 1) + '*');
+                Console.WriteLine(document.ShortName());
+            }
+
+            foreach (var subBranch in branch.Branches())
+            {
+                Console.Write(new string(' ', indent) + '-');
+                Console.WriteLine(subBranch.Tag().Text() + ":");
+
+                ShowContent(branch, indent + 1);
+            }
+        }
+    }
+}
---
+++ document-tagging-system/ITagBranch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace document_tagging_system
+{
+    public interface ITagBranch
+    {
+        ITag Tag();
+        /// <returns>branch for each child tag of this branch's tag</returns>
+        IEnumerable<ITagBranch> Branches();
+        /// <returns>all documents with this branch's tag as a full tag</returns>
+        IEnumerable<IDocument> Documents();
+    }
+}
